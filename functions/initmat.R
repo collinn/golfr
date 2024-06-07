18ea84b85b04d481f9af2baf2ae669394edb3c93
Hannah Kim <kimhanna3@grinnell.edu>
2024-06-07 13:29:54 -0500

    functions separated as .R

--- functions/initmat.R
+++ functions/initmat.R
@@ -1,10 +1,9 @@
 # initmat
-## Takes integer as input, returns matrix
+## Takes column of the data frame as input, returns matrix
 ### Generates initial matrix
-initMat <- function(num_students) {
-  n <- length(num_students)
-  initMat <- matrix(0, nrow = n, ncol = n, dimnames = list(students, students))
-  diag(initMat) <- -99
-  # -99 can be replaced with -(n-1) when n = number of rows (students)
-  return(InitMat)
+initmat <- function(students) {
+  n <- length(students)
+  initmat <- matrix(0, nrow = n, ncol = n, dimnames = list(students, students))
+  diag(initmat) <- -99
+  return(initmat)
 }
--- functions/updatemat.R
+++ functions/updatemat.R
@@ -2,11 +2,11 @@
 ## Takes matrix, vector and integer as inputs, returns matrix
 ### Updates the results of first round to the initial matrix
 
-updatemat <- function(initmat, groupAssignments, students) {
-  numGroups <- length(unique(groupAssignments))
+updatemat <- function(initmat, group_assignments, students) {
+  numGroups <- length(unique(group_assignments))
   
   for (g in seq_len(numGroups)) {
-    groupMembers <- students[groupAssignments == g,]
+    groupMembers <- students[group_assignments == g,]
     for (j in seq_along(groupMembers)) {
       for (k in seq_along(groupMembers)) {
         if (j != k) {
