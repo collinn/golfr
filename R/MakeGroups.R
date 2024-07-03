b4d9b1f9fffe54a05f1baf1724f5bedab505c2d5
Hannah Kim <kimhanna3@grinnell.edu>
2024-07-03 11:31:37 -0500

    documentation added

--- R/MakeGroups.R
+++ R/MakeGroups.R
@@ -1,8 +1,22 @@
-# MakeGroups
-## Takes data frame and two integers as inputs, returns data frame
-### Assign every student into groups with set number of students per group,
-### with set number of iterations (overlap not considered)
-
+#'  Assign every student into groups with set number of students per group
+#'
+#' @description
+#'  Assign every student into groups with set number of students per group,
+#'  with set number of iterations (overlap not considered)
+#'
+#' @param data a data frame
+#' @param students_per_group a positive integer
+#' @param iterations a positive integer
+#' @param initial_matrix matrix generated with `initmat()`
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' # Assign 9 students into 3 groups of 3, with 3 iterations
+#' data <- GenerateData(9)
+#' M <- initmat(data$Student)
+#' MakeGroups(data, 3, 3, M)
 MakeGroups <- function(data, students_per_group, iterations, initial_matrix) {
   num_students <- nrow(data)
   # ceiling or floor?
@@ -10,18 +24,18 @@ MakeGroups <- function(data, students_per_group, iterations, initial_matrix) {
   # Initialize a list to store group assignments for each iteration
   group_assignments_list <- list()
   group_assignments_list <- vector("list", length = iterations)
-  
+
   # Initialize the matrix for each iteration
   initial_matrix <- initmat(data$Student)
-  
+
   for (i in seq_len(iterations)) {
     shuffled_students <- sample(data$Student)
-    
+
     # Create initial group assignments
-    group_assignments <- rep(seq_len(num_groups), 
-                             each = students_per_group, 
+    group_assignments <- rep(seq_len(num_groups),
+                             each = students_per_group,
                              length.out = num_students)
-    
+
     ## This will always put extra students in existing groups
     ## Maybe want to create a new group if rem_stud/n_group > 1/2
     # Distribute any remaining students among the existing groups
@@ -47,14 +61,14 @@ MakeGroups <- function(data, students_per_group, iterations, initial_matrix) {
       }
     }
     # Add group assignments to the list
-    iteration_groups <- data.frame(Student = shuffled_students, 
+    iteration_groups <- data.frame(Student = shuffled_students,
                                    round = group_assignments)
     names(iteration_groups) <- c("Student", paste0("Round", i))
     group_assignments_list[[i]] <- iteration_groups
   }
-  
+
   # Combine all data frames for each round into a single data frame
   combined_data <- Reduce(function(x, y) merge(x, y, by = "Student", all = TRUE), group_assignments_list, init = data.frame(Student = data$Student))
-  
+
   return(combined_data)
 }
